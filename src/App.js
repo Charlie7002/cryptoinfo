baca8433557290d805323921161fed241e96bc2f
charlie7002 <chaced@orange.fr>
2021-12-04 21:44:20 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import {
+	Cryptocurrencies,
+	CryptoDetails,
+	Exchanges,
+	Homepage,
+	Navbar,
+	News,
+} from './components';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Layout, Space, Typography } from 'antd';
+const App = () => {
+	return (
+		<div className="app">
+			<div className="navBar">
+				<Navbar />
+			</div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						<Routes>
+							<Route path="/" exact element={<Homepage />} />
+							<Route path="/exchanges" exact element={<Exchanges />} />
+							<Route
+								path="/cryptocurrencies"
+								exact
+								element={<Cryptocurrencies />}
+							/>
+							<Route
+								path="/crypto/:coinId"
+								exact
+								element={<CryptoDetails />}
+							/>
+							<Route path="/crypto/news" exact element={<News />} />
+						</Routes>
+					</div>
+				</Layout>
+				<div className="footer">
+					<Typography.Title
+						level={5}
+						style={{ color: 'white', textAlign: 'center' }}
+					>
+						Crytoverse
+						<br />
+						All right reseverd
+					</Typography.Title>
+					<Space>
+						<Link to="/">Home</Link>
+						<Link to="/exchanges">Exchanges</Link>
+						<Link to="/news">News</Link>
+					</Space>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { icons } from 'antd/lib/image/PreviewGroup';
+import React from 'react';
+import {
+	HomeOutlined,
+	MoneyCollectOutlined,
+	FundOutlined,
+	BulbOutlined,
+	MenuOutlined,
+} from '@ant-design/icons';
+import { Avatar, Button, Typography, Menu } from 'antd';
+import icon from '../images/cryptocurrency.png';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+	return (
+		<div className="nav-container">
+			<div className="logo-container">
+				<Avatar src={icon} size="large" />
+				<Typography.Title level={2} className="logo">
+					<Link to="/">CryptoFuck</Link>
+				</Typography.Title>
+			</div>
+			<Menu theme="dark">
+				<Menu.Item icon={<HomeOutlined />}>
+					<Link to="/">Home</Link>
+				</Menu.Item>
+				<Menu.Item icon={<FundOutlined />}>
+					<Link to="/cryptocurrencies">Cryptocurrencies</Link>
+				</Menu.Item>
+				<Menu.Item icon={<MoneyCollectOutlined />}>
+					<Link to="/exchanges">Exchanges</Link>
+				</Menu.Item>
+				<Menu.Item icon={<BulbOutlined />}>
+					<Link to="/news">News</Link>
+				</Menu.Item>
+			</Menu>
+			{/* <Button className="menu-control-container">but</Button> */}
+		</div>
+	);
+};
+
+export default Navbar;
