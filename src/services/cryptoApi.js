931b0fc5cbcdc0f9d7e002a68b86aa3736a76c6a
charlie7002 <chaced@orange.fr>
2021-12-04 21:50:27 +0100

    del env

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,8 +2,8 @@ import dotenv from 'dotenv';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoAPiHeaders = {
-	'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
 	'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
+	'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
 };
 
 const baseUrl = process.env.REACT_APP_CRYPTO_API_URL;
